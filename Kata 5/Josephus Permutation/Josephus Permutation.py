f4ff33727bac37a6a9612fa4314acd67b127272c
Eddie02582 <38853252+Eddie02582@users.noreply.github.com>
2019-05-28 18:11:45 +0800

    Update

--- Kata 5/Josephus Permutation/Josephus Permutation.py
+++ Kata 5/Josephus Permutation/Josephus Permutation.py
@@ -5,3 +5,11 @@ def josephus(items, k):
         result.append(items.pop(i))
     return result
 	
+    
+def josephus(items,k):
+    reuslt,index=[],0    
+    while items:   
+        index=(index+k-1)%len(items)     
+        reuslt.append(items[index])
+        items.pop(index)        
+    return reuslt
